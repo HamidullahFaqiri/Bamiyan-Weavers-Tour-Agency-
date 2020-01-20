3ab2c227281dab2f049f542f9f2d04b218aae14d
HamidullahFaqiri <khamidfakiri@gmail.com>
2020-01-20 16:01:53 +0300

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\AMG\Desktop\Курсовой проект по веб программированию\Projectweb\Projectweb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
